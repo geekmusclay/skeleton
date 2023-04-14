70f52cf9540411c3ec1b268871e4ef93163c4ad1
cedric <cedric@vision-si.com>
2023-04-14 14:00:17 +0200

    master: refactor controllers

---
+++ src/Controller/Api/ArticleController.php
@@ -0,0 +1,42 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Model\Article;
+use Geekmusclay\Router\Attribute\Route;
+use Geekmusclay\Router\Core\JsonResponse;
+use Geekmusclay\Framework\Common\AbstractController;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+/**
+ * Sample Article API controller
+ */
+#[Route(path: '/api/v1/article')]
+final class ArticleController extends AbstractController
+{
+    /**
+     * Controller index function.
+     *
+     * @param Request $request The server request
+     */
+    #[Route(path: '/', name: 'article.api.index')]
+    public function index(Request $request): JsonResponse
+    {
+        $limit = 10;
+
+        /** @var string[] $params Potential request GET params */
+        $params = $request->getQueryParams();
+        if (true === isset($params['limit'])) {
+            $limit = (int) $params['limit'];
+        }
+
+        $res = [];
+        foreach (Article::all($limit) as $article) {
+            $res[] = $article->serialize();
+        }
+
+        return $this->json($res);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,47 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Model\Article;
 use Geekmusclay\Framework\Common\AbstractController;
-use Geekmusclay\Framework\Renderer\TwigRenderer;
-use Geekmusclay\Router\Core\JsonResponse;
 use GuzzleHttp\Psr7\Response;
 use Geekmusclay\Router\Attribute\Route;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
+/**
+ * Sample HomeController class
+ */
 final class HomeController extends AbstractController
 {
     /**
-     * @param TwigRenderer $renderer The controller renderer
-     */
-    public function __construct(TwigRenderer $renderer)
-    {
-        parent::__construct($renderer);
-    }
-
-    /**
-     * Controller index route.
+     * Sample controller route index.
      */
     #[Route(path: '/', name: 'app.home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
-    }
-
-    #[Route(path: '/api', name: 'app.api')]
-    public function api(Request $request): JsonResponse
-    {
-        $limit = 10;
-        $params = $request->getQueryParams();
-        if (true === isset($params['limit'])) {
-            $limit = (int) $params['limit'];
-        }
-
-        $res = [];
-        foreach (Article::all($limit) as $article) {
-            $res[] = $article->serialize();
-        }
-
-        return $this->json($res);
     }
 }
