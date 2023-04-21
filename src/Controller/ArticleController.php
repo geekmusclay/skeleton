d514d431f21e6091ffe410d15777392d9c9a26e0
cedric <cedric@vision-si.com>
2023-04-21 14:56:22 +0200

    workflow: added missing routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,24 +4,69 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Throwable;
 use App\Model\Article;
 use GuzzleHttp\Psr7\Response;
 use Geekmusclay\Router\Attribute\Route;
+use Geekmusclay\Framework\Core\Encrypter;
+use Psr\Http\Message\ServerRequestInterface;
+use Geekmusclay\Router\Interfaces\RouterInterface;
 use Geekmusclay\Framework\Common\AbstractController;
+use Geekmusclay\Framework\Interfaces\RendererInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-
-#[Route(path: '/article')]
+#[Route(path: '/articles')]
 final class ArticleController extends AbstractController
 {
-    #[Route(path: '/:id', name: 'article.details', with: [
-        'id' => '[0-9]+'
-    ])]
-    public function detail(int $id): Response
+    /** @var Encrypter $encrypter Contain the application encryption service */
+    private Encrypter $encrypter;
+
+    /** @var RendererInterface $renderer Defdault renderer */
+    protected RendererInterface $renderer;
+
+    /** @var RouterInterface $router Application router */
+    protected RouterInterface $router;
+
+    /**
+     * Controller constructor
+     *
+     * @param RendererInterface $renderer  Defdault renderer
+     * @param RouterInterface   $router    Application router
+     * @param Encrypter         $encrypter The application encryption service
+     */
+    public function __construct(
+        RendererInterface $renderer,
+        RouterInterface   $router,
+        Encrypter         $encrypter
+    ) {
+        $this->renderer  = $renderer;
+        $this->router    = $router;
+        $this->encrypter = $encrypter;
+    }
+
+    #[Route(path: '/', name: 'article.index')]
+    public function index(): Response
     {
-        $article = Article::find($id);
+        $articles = Article::all();
+
+        return $this->render('artiicles/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    #[Route(path: '/:id', name: 'article.details', with: [
+        'id' => '[a-zA-Z0-9]+'
+    ])]
+    public function detail(ServerRequestInterface $request, string $id): Response
+    {
+        $decrypt = $this->encrypter->decrypt($id);
+        if (-1 === $decrypt) {
+            return $this->redirect($request, 'app.home');
+        }
+
+        $article = Article::find($decrypt);
         if (false === $article) {
-            // Error
+            return $this->redirect($request, 'app.home');
         }
 
         return $this->render('articles/details.html.twig', [
@@ -48,7 +93,66 @@ final class ArticleController extends AbstractController
         }
 
         $article = new Article($data);
-        // $article->save() @TODO implement this
+        try {
+            $article->save();
+        } catch (Throwable $e) {
+            // Do something with the error
+
+            return $this->redirect($request, 'app.home');
+        }
+
+        return $this->render('articles/store.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    #[Route(method: 'GET', path: '/edit/:id', name: 'article.edit', with: [
+        'id' => '[a-zA-Z0-9]+'
+    ])]
+    public function edit(Request $request, string $id): Response
+    {
+        $decrypt = $this->encrypter->decrypt($id);
+        if (-1 === $decrypt) {
+            return $this->redirect($request, 'app.home');
+        }
+
+        $article = Article::find($decrypt);
+        if (null === $article) {
+            return $this->redirect($request, 'app.home');
+        }
+
+        return $this->render('articles/store.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    #[Route(method: 'POST', path: '/update/:id', name: 'article.update', with: [
+        'id' => '[a-zA-Z0-9]+'
+    ])]
+    public function update(Request $request, string $id): Response
+    {
+        $decrypt = $this->encrypter->decrypt($id);
+        if (-1 === $decrypt) {
+            return $this->redirect($request, 'app.home');
+        }
+
+        $data = $request->getParsedBody();
+        if (null === $data || 0 === count($data)) {
+            return $this->render('articles/edit.html.twig', [
+                'errors' => [
+                    'Wrong form'
+                ]
+            ]);
+        }
+
+        $article = new Article($data);
+        try {
+            $article->save();
+        } catch (Throwable $e) {
+            // Do something with the error
+
+            return $this->redirect($request, 'app.home');
+        }
 
         return $this->render('articles/store.html.twig', [
             'article' => $article
