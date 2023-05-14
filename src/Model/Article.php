a0083a3c9761b496bda6b7d0a06cdcf3f24c23f6
Cédric Noël <cedric.noel.paris@gmail.com>
2023-05-14 16:55:50 +0200

    master: added default values to article

--- src/Model/Article.php
+++ src/Model/Article.php
@@ -8,5 +8,12 @@ use Geekmusclay\ORM\Entity\Model;
 
 class Article extends Model
 {
-    // Some code here ...
+    /** @var string $title The title of the article */
+    private string $title = 'Default title';
+
+    /** @var string $subtitle The subtitle of the article */
+    private string $subtitle = 'Default subtitle';
+
+    /** @var string $body The body of the article */
+    private string $body = 'Default body';
 }
